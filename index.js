ca465d963f9a7e71a5b7419fef84705a17d90dfa
Areeb Shaikh <arshaikh@iu.edu>
2024-02-05 20:58:46 +0000

    v2 complete

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+const {Storage} = require('@google-cloud/storage');
+const { error } = require('console');
+const csv = require('csv-parser');
+
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+    // console.log(`  Event: ${context.eventId}`);
+    // console.log(`  Event Type: ${context.eventType}`);
+    // console.log(`  Bucket: ${file.bucket}`);
+    // console.log(`  File: ${file.name}`);
+
+    const gcs = new Storage();
+
+    const dataFile = gcs.bucket(file.bucket).file(file.name);
+
+    dataFile.createReadStream()
+
+    .on('error', () => {
+        //Handle the error
+        console.error(error);
+    })
+    .pipe(csv())
+
+    .on('data', (row) => {
+        //Log row data
+        console.log(row);
+    })
+    
+    .on('end', () => {
+        //Handle end of csv
+        console.log("CSV End!");
+    })
 }
 
